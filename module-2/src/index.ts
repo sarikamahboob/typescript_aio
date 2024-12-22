d306fa503545fbb08333251014b599404fc4fa29
sarikamahboob <sarika.mahboob1234@gmail.com>
2024-12-22 13:24:01 +0600

    feat: module-2(2.2) added

--- module-2/src/index.ts
+++ module-2/src/index.ts
@@ -1,2 +1,7 @@
-var names = "sarika"
-console.log(names)
+let course: string = "front-end";
+course = "back-end";
+const age: number = 20
+const isAdmin : boolean = false
+console.log(course)
+
+// primitive types - bigint, boolean, string, number, undefined, null, symbol 
