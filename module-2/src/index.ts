5af7ffd89371ae8dd702ea1930a5b3f60b4b9df9
sarikamahboob <sarika.mahboob1234@gmail.com>
2024-12-23 15:25:47 +0600

    feat: module-2(2.3) added

--- module-2/src/index.ts
+++ module-2/src/index.ts
@@ -5,3 +5,11 @@ const isAdmin : boolean = false
 console.log(course)
 
 // primitive types - bigint, boolean, string, number, undefined, null, symbol 
+
+// array types
+const primitiveTypes: string[] = ["bigint", "boolean", "string"]
+const rollNumbers:number[] = [2,3,4,5]
+
+// tuple types
+const user: [number, string] = [4, "sarika"]
+const player: [string, boolean] = ["malinga", true]
